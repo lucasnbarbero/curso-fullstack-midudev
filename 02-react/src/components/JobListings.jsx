f537e64a8baf2b9844d71fc612a30cae8660788d
Lucas Barbero <lucasnbarbero95@gmail.com>
2026-08-19 13:12:33 -0300

    feat: scaffold initial DevJobs project structure with layout components and job data

---
+++ 02-react/src/components/JobListings.jsx
@@ -0,0 +1,11 @@
+export function JobListings() {
+  return (
+    <>
+      <h2>Resultados de búsqueda</h2>
+
+      <div className="jobs-listings">
+        {/* Aquí se insertan los empleos dinámicamente */}
+      </div>
+    </>
+  );
+}
